62e1bc439ccb38f21ab8fd5eddeb80b5ed884be4
Cristian Clavijo <cristian982940@gmail.com>
2019-05-14 23:12:16 -0500

    create component Header and Formulario and show in component App.js

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import '../css/App.css';
+import Header from './Header';
+import Formulario from './Formulario';
+
 
 function App() {
   return (
-    <h1>Hola</h1>
+    <div className="App container">
+      <Header
+        titulo = 'Gasto Semanal'
+      />
+      <div className="contenido-principal contenido">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario/>
+          </div>
+
+          <div className="one-half column">
+
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
